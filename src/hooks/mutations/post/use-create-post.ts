34c9a074207d6ffacc278cd474696163972ea44e
soundraven <kin9709@gmail.com>
2026-01-15 20:29:53 +0900

    indexPage에 무한 스크롤 구현, react-intersection-observer 라이브러리 사용

--- src/hooks/mutations/post/use-create-post.ts
+++ src/hooks/mutations/post/use-create-post.ts
@@ -1,4 +1,4 @@
-import { createPost, createPostWithImages } from "@/api/post";
+import { createPostWithImages } from "@/api/post";
 import { QUERY_KEYS } from "@/lib/constants";
 import type { UseMutationCallback } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
